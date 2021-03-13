9e6d5aec54014c819a16ea125d8ce30d3dcc28e7
Logan Lawrence <llawrence@logan-lawrence-workstation.localdomain>
2021-03-12 20:18:13 -0500

    basic image collecting from google

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # dataset-collection
 UC San Diego research project
+
+### Image Collector
+Queries for the given terms, stores in a given directory, and then returns a list of numpy arrays where each numpy array is a 3D representation of an image (height-width-RGB). 
+
+Main function is given by:
+`def search_store_query(search: str, num: int, dir_name: str = None, options:dict = {}) -> None:`
+Where:
+- *search* is the string you would input into Google to find the images
+- *num* is the amount of images
+- *dir_name* is the directory you would like to save in (will generate a directory if not given)
+- *options* is misc. options like fileType, imgType, imgSize, etc. See code to become familiar
+
+#### Google Image Searches
+Max queries per day is 100 without a pricing plan. Above this, it's $5 per 1000 queries. We may be able to get this funding from the department.
+Ping lclawren@eng.ucsd.edu for API and CX key, or just message me in Slack.
+
+### NOTES
+Easy installation:
+`pip install -r requirements.txt`
---
+++ image-collector/query_google.py
@@ -0,0 +1,54 @@
+from google_images_search import GoogleImagesSearch
+from PIL import Image
+from io import BytesIO
+
+import numpy as np
+import pathlib
+import os
+import typing
+import datetime
+import json
+
+gis = GoogleImagesSearch(os.environ.get('GCS_DEVELOPER_KEY'), os.environ.get('GCS_CX'))
+my_bytes_io = BytesIO()
+
+def search_store_query(search: str, num: int, dir_name: str = None, options:dict = {}) -> None:
+    dir_path = dir_name
+    if not dir_path: 
+        cur_time = datetime.datetime.now()
+        name = 'dataset-' + search + '_' + cur_time.strftime("%m-%d-%YT%H:%M:%S")
+        dir_path = str(pathlib.Path(__file__).parent.parent.absolute()) + '/' + name
+
+    _search_params = {
+        'q': search,
+        'num': num,
+        'fileType': options.get('fileType', 'jpg'),
+        'imgType': options.get('imgType', 'photo'),
+        'imgSize': options.get('imgSize', 'LARGE'),
+    }
+
+    gis.search(search_params=_search_params, path_to_dir=dir_path, width=options.get('dim', 500), height=options.get('dim', 500))
+
+    try:
+        file = open(dir_path + "/query.json", "w")
+        json_formatted_str = json.dumps(_search_params, indent=2)
+        file.write(json_formatted_str)
+        file.close()
+    except:
+        raise Exception('error writing to file')
+
+    X = []
+    for _ in range(int(num / 10) + 1):
+        for image in gis.results():
+            my_bytes_io.seek(0)
+            image.copy_to(my_bytes_io)
+            my_bytes_io.seek(0)
+
+            data = np.asarray(Image.open(my_bytes_io))
+            X.append(data)
+        gis.next_page()
+    
+    # return np.stack(X, axis=3)
+    return X
+
+search_store_query('dog', 2)
