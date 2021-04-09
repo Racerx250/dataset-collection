65e74e191688be98a45dafa3c2ccb1886bc0acc4
Logan Lawrence <llawrence@logan-lawrence-workstation.localdomain>
2021-04-09 17:57:06 -0400

    finished dog dataset builder

--- build_dog_dataset.py
+++ build_dog_dataset.py
@@ -7,16 +7,26 @@ dataset_name = 'dataset_' + 'dogs' + '_' + cur_time.strftime("%m-%d-%YT%H:%M:%S"
 
 classes = [
     'beagle',
-    # 'border collie',
-    # 'miniature schnauzer',
-    # 'boston terrier',
-    # 'labradoodle',
+    'border collie',
+    'miniature schnauzer',
+    'boston terrier',
+    'labradoodle',
+    'chihuahua',
+    'golden retriever',
+    'french bulldog',
+    'poodle',
+    'pembroke welsh corgi',
+    'german shepherd',
+    'great dane',
+    'shiba inu',
+    'dalmatian',
+    'labrador retriever'
 ]
 for label in classes:
     print('Starting Flickr scrape of ' + label + '.')
-    flickr_dir = dataset_name + '/' + label + '/flickr'
-    search_store_query_flickr(label + ' dog', dir_name=flickr_dir, num_images=100, use_subfolder=False)
+    flickr_dir = dataset_name + '/' + label.replace(' ', '_') + '/flickr'
+    search_store_query_flickr(label + ' dog', dir_name=flickr_dir, num_images=80, use_subfolder=False)
     
     print('Starting Google scrape of ' + label + '.')
-    google_dir = dataset_name + '/' + label + '/google'
-    search_store_query_google(label + ' dog', 10, dir_name=google_dir)
+    google_dir = dataset_name + '/' + label.replace(' ', '_') + '/google'
+    search_store_query_google(label + ' dog', 80, dir_name=google_dir)
