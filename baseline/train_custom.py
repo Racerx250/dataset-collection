6acd3ba828aec7063e4f7d3b581fd03534c95762
gtz05050708 <q6zhang@ucsd.edu>
2021-04-22 09:46:12 -0700

    update training code

--- baseline/train_custom.py
+++ baseline/train_custom.py
@@ -2,11 +2,13 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import datasets
+import dogData
 from matplotlib import pyplot as plt
 import torchvision.models as models
 from collections import defaultdict
 import custom
+from torchvision import transforms as transforms
+from torchvision import datasets as datasets
 
 # xavier initialization
 def weights_init(m):
@@ -36,7 +38,6 @@ def graph(val_loss, train_loss, label):
     plt.show()
 
 def train_model(model, dataloaders, epochs, optimizer, criterion, patience) :
-    model.apply(weights_init)
     train_dataloader = dataloaders['train']
     val_dataloader = dataloaders['validate']
     test_dataloader = dataloaders['test']
@@ -75,6 +76,8 @@ def train_model(model, dataloaders, epochs, optimizer, criterion, patience) :
             optimizer.step()
             train_loss += loss.item()
 
+        print("epoch train complete")
+
         # validate 
         model = model.eval()
         with torch.no_grad():
@@ -91,6 +94,8 @@ def train_model(model, dataloaders, epochs, optimizer, criterion, patience) :
                 val_loss += loss.item()
         model = model.train()
 
+        print("epoch validate complete")
+
         train_loss_total.append(train_loss / batch)
         val_loss_total.append(val_loss / batch_valid)
 
@@ -101,6 +106,7 @@ def train_model(model, dataloaders, epochs, optimizer, criterion, patience) :
             loss_increase += 1
             if loss_increase >= patience :
                 torch.save(model, './saved_model')
+                print ("early stopping")
                 break
         else :
             loss_increase = 0
@@ -124,27 +130,48 @@ def train_model(model, dataloaders, epochs, optimizer, criterion, patience) :
 
     print("Accuracy of the network: " + str(test_acc.cpu().numpy()/len(test_dataset)))
 
+if __name__ == '__main__': 
+    model = models.vgg16(pretrained=True)
+   # model.fc = nn.Linear(512,120)
+   # model.fc.apply(weights_init)
 
-model = custom.Custom_Net(classes = 20)
+    model.classifier[6] = nn.Linear(4096,120)
+    model.classifier[6].apply(weights_init)
 
-# load the datasets
-root = 'birds_dataset/'
-train_dataset = datasets.bird_dataset(root,'train_list.txt')
-test_dataset = datasets.bird_dataset(root,'test_list.txt')
-valid_dataset = datasets.bird_dataset(root,'valid_list.txt')
+    
+    # load the datasets
+    '''
+    train_dataset = dogData.Dog_Train_Dataset('train_list.txt')
+    valid_dataset = dogData.Dog_Test_Dataset('val_list.txt')
+    test_dataset = dogData.Dog_Test_Dataset('test_list.txt')
+    '''
+    
+    train_transform = transforms.Compose([ 
+        transforms.Resize(256), 
+        transforms.RandomCrop(224), 
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-# dataloaders for the datasets
-train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size = 8)
-test_dataloader = DataLoader(test_dataset, shuffle=False)
-valid_dataloader = DataLoader(valid_dataset, shuffle=False)
+    test_transform = transforms.Compose([ 
+        transforms.Resize(256), 
+        transforms.CenterCrop(224), 
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-dataloaders = defaultdict(DataLoader)
-dataloaders['train'] =  train_dataloader
-dataloaders['validate'] =  valid_dataloader
-dataloaders['test'] =  test_dataloader
+    train_dataset= datasets.ImageFolder('TrainImages/', transform=train_transform)
+    valid_dataset= datasets.ImageFolder('ValImages/', transform=test_transform)
+    test_dataset= datasets.ImageFolder('TestImages/', transform=test_transform)
 
-epochs = 2
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-criterion = nn.CrossEntropyLoss()
-model = model.cuda()
-train_model(model, dataloaders, epochs, optimizer, criterion, 5)
+    #train_dataset, valid_dataset, test_dataset = torch.utils.data.random_split(total_dataset, [14406, 3087, 3087])
+
+    # dataloaders for the datasets
+    dataloaders = defaultdict(DataLoader)
+    dataloaders['train'] =  DataLoader(train_dataset, shuffle=True, batch_size = 8, num_workers=4)
+    dataloaders['validate'] =  DataLoader(valid_dataset, shuffle=False, num_workers=4)
+    dataloaders['test'] =  DataLoader(test_dataset, shuffle=False, num_workers=4)
+
+    epochs = 10
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    criterion = nn.CrossEntropyLoss()
+    model = model.cuda()
+    train_model(model, dataloaders, epochs, optimizer, criterion, 2)
